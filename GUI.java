cc9c4a187d286c8714fcd6ce89639ef5d6b1b589
ryansutc <ryansutc@gmail.com>
2016-04-14 10:01:30 -0300

    Create GUI.java
    initial beta version. Working moving objects with no special effects

---
+++ GUI.java
@@ -0,0 +1,66 @@
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Color;
+
+
+public class GUI {
+
+	private JFrame frame;
+	private JPanel contentPane;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		frame.setContentPane(contentPane);
+		contentPane.setLayout(new CardLayout(0,0));
+		Canvas canvas = new Canvas();
+		canvas.setBackground(new Color(255, 218, 185));
+		canvas.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				System.out.println("Mouse clicked");
+				canvas.addShape(arg0.getX(),arg0.getY());
+				
+			}
+		});
+		contentPane.add(canvas);
+	}
+
+}
